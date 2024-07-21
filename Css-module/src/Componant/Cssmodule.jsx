61786da4b753b907bcaa183df0e5be3fe321d6d6
Chhaya Mishra <141479837+chhaya03@users.noreply.github.com>
2024-07-21 19:24:30 +0530

    Update Cssmodule.jsx

--- Css-module/src/Componant/Cssmodule.jsx
+++ Css-module/src/Componant/Cssmodule.jsx
@@ -3,11 +3,11 @@ import styles from "./item.module.css";
 function Cssmodule() {
   return (
     <div>
-      <p className={styles["heading"]}>
+      <p className={styles.heading}>
         In this page, we learn about Css module
       </p>
 
-      <p className={styles["paragraph"]}>
+      <p className={styles.paragraph}>
         It is used for uniqueness , if same properies are defines in different
         file of react so during compilation for avoiding clashing between same
         properties, we can use css module
